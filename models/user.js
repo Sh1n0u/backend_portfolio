76a5605d72dc7b764503f5a30b95ed76c283ad39
Sh1n0u <florian.sune@gmail.com>
2023-09-04 09:10:07 +0200

    remove admin params

--- models/user.js
+++ models/user.js
@@ -4,7 +4,6 @@ const uniqueValidator = require('mongoose-unique-validator');
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    admin: { type: Boolean, default: false },
 });
 
 userSchema.plugin(uniqueValidator);
