f03129174bfbe5a50f01adba905572e8a816ad3f
Sh1n0u <florian.sune@gmail.com>
2023-09-05 12:04:25 +0200

    add controller Update + delete

--- controller/project-controller.js
+++ controller/project-controller.js
@@ -36,50 +36,57 @@ exports.getAllProject = (request, response, next) => {
 };
 
 exports.updateProject = (request, response, next) => {
-    const projectId = request.params.id;
+    const projectObject = request.file
+        ? {
+              ...JSON.parse(request.body.project),
+              imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+          }
+        : { ...request.body };
 
-    Project.findById(projectId)
+    delete projectObject._userId;
+    Project.findOne({ _id: request.params.id })
         .then((project) => {
-            // Affichage d'erreur si le projet n'existe pas
-            if (!project) {
-                return response.status(404).json({ message: 'Projet non trouvé' });
+            console.log('request.auth.userId:', request.auth.userId);
+            console.log('project.userId:', project.userId);
+            if (project.userId != request.auth.userId) {
+                response.status(401).json({ message: 'Non autorisé' });
+            } else {
+                Project.updateOne({ _id: request.params.id }, { ...projectObject, _id: request.params.id })
+                    .then(() => response.status(200).json({ message: 'Projet modifié' }))
+                    .catch((error) => response.status(401).json({ error }));
             }
-
-            // Sauvegarder le chemin de l'ancien fichier image
-            const oldImagePath = path.join(process.env.IMAGE_DIR, path.basename(project.imageUrl));
-
-            // Met à jour le projet avec les nouvelles données
-            if (request.body.title) {
-                project.title = request.body.title;
-            }
-
-            if (request.body.description) {
-                project.description = request.body.description;
-            }
-
-            // Met à jour le projet avec la nouvelle image si elle est fournie
-            if (request.file) {
-                project.imageUrl = `${request.protocol}://${request.get('host')}/images/${request.file.filename}`;
-            }
-
-            // Enregistrez le projet mis à jour
-            return project.save();
-        })
-        .then(() => {
-            // Supprime l'ancienne image après avoir enregistré
-            if (request.file && fs.existsSync(oldImagePath)) {
-                fs.unlinkSync(oldImagePath, (error) => {
-                    if (error) {
-                        console.error("Erreur lors de la suppression de l'ancienne image", error);
-                    }
-                });
-            }
-
-            response.status(200).json({ message: 'Projet mis à jour' });
         })
         .catch((error) => {
-            const imagePath = path.join(process.env.IMAGE_DIR, request.file.name);
-            fs.unlinkSync(imagePath);
-            response.status(400).json({ error });
+            res.status(400).json({ error });
         });
 };
+
+exports.deleteProject = (request, response, next) => {
+    const projectId = request.params.id;
+
+    Project.findOne({ _id: projectId})
+        .then((project) => {
+            // Vérification de la présence du projet
+            if (!project) {
+                return response(404).json({ message: 'Projet non trouvé'})
+            }
+
+            // Vérification de l'autorisation du user
+            if (project.userId !== request.auth.userId) {
+                response.status(401).json({ message: 'Non autorisé'})
+            }
+
+            // Suppression de l'image du projet
+            const imagePath = path.join(process.env.IMAGE_DIR, project.imageUrl.split('/images')[1])
+            fs.unlinkSync(imagePath)
+
+            // Suppression du projet
+            Project.deleteOne({ _id: projectId})
+                .then(() => {
+                    response.status(200).json({ message: 'Projet supprimé avec succès'})
+                })
+                .catch((error) => {
+                    response.status(400).json({ error })
+                })
+        })
+}
