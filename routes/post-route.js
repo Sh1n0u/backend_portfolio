8a9aa664135244adb6a558678207c832080df612
Sh1n0u <florian.sune@gmail.com>
2023-09-07 09:32:09 +0200

    add getAll route

--- routes/post-route.js
+++ routes/post-route.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const postController = require('../controller/post-controller')
 
 router.post('/posts', postController.createPost)
+router.post('/posts', postController.getAllPost)
 
 module.exports = router
