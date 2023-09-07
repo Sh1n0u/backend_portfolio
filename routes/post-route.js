65af88618405f50a852390df7ccf108fd5d7c6fc
Sh1n0u <florian.sune@gmail.com>
2023-09-07 17:18:29 +0200

    fix error

--- routes/post-route.js
+++ routes/post-route.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const postController = require('../controller/post-controller')
 
 router.post('/posts', postController.createPost)
-router.post('/posts', postController.getAllPost)
+router.get('/posts', postController.getAllPost)
 
 module.exports = router
