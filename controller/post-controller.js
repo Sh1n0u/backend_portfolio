0399dfdf3d39418ddc9b808129e4967b280a1095
Sh1n0u <florian.sune@gmail.com>
2023-09-07 09:31:27 +0200

    add getAllPost controller

--- controller/post-controller.js
+++ controller/post-controller.js
@@ -1,18 +1,27 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
 
 exports.createPost = (request, response, next) => {
     const post = new Post({
-        email: request.boby.email,
+        email: request.body.email,
         message: request.body.message,
         userId: request.userId,
-    })
+    });
 
-    Post
-        .save()
+    post.save()
         .then(() => {
-            response.status(201).json({ message: 'Message envoyé'})
+            response.status(201).json({ message: 'Message envoyé' });
         })
         .catch((error) => {
-            response.status(400).json({ error })
+            response.status(400).json({ error });
+        });
+};
+
+exports.getAllPost = (request, response, next) => {
+    Post.find()
+        .then((post) => {
+            response.status(200).json(post);
         })
-}
+        .catch((error) => {
+            response.status(500).json({ error });
+        });
+};
